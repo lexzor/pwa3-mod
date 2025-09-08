1d300d4d4aba42b69ad11a3feb0fa9193f9b04b7
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2025-09-08 17:53:05 +0300

    Added Logger static class

---
+++ src/utils/logger.h
@@ -0,0 +1,129 @@
+#pragma once
+
+#include <string>
+#include <format>
+#include <print>
+#include <algorithm>
+#include <array>
+
+constexpr int COLORS_COUNT = 8;
+
+class Logger
+{
+public:
+    enum class TextColor
+    {
+        Default,
+        Red,
+        Green,
+        Yellow,
+        Blue,
+        Purple,
+        Cyan,
+        White,
+    };
+
+    struct Options
+    {
+        std::string ColorPrefix;
+        std::string InfoPrefix;
+        std::string WarningPrefix;
+        std::string ErrorPrefix;
+    };
+
+private:
+    static Options m_Options;
+    static std::array<std::string, COLORS_COUNT> TextColorsKeys;
+    static std::array<std::string, COLORS_COUNT> TextColors;
+
+public:
+    static void SetOptions(const Options& options) {
+        m_Options = options;
+
+        AddColor(m_Options.InfoPrefix);
+        AddColor(m_Options.WarningPrefix);
+        AddColor(m_Options.ErrorPrefix);
+    }
+
+	template<typename ...Args>
+    static void Info(std::string_view fmt, Args&&... args)
+    {
+        std::string text = std::vformat(fmt, std::make_format_args(args...));
+        AddColor(text);
+
+        std::cout << m_Options.InfoPrefix + text << "\n";
+    }
+
+    template<typename ...Args>
+    static void Warning(std::string_view fmt, Args&&... args)
+    {
+        std::string text = std::vformat(fmt, std::make_format_args(args...));
+        AddColor(text);
+       
+        std::cout << m_Options.WarningPrefix + text  << "\n";
+    }
+
+    template<typename ...Args>
+    static void Error(std::string_view fmt, Args&&... args)
+    {
+        std::string text = std::vformat(fmt, std::make_format_args(args...));
+        AddColor(text);
+
+        std::cout << m_Options.ErrorPrefix + text << "\n";
+    }
+
+private:
+    static void AddColor(std::string& text)
+    {
+        size_t pos = 0;
+
+        while ((pos = text.find(m_Options.ColorPrefix, pos)) != std::string::npos)
+        {
+            if (pos + 1 >= text.size()) break;
+
+            std::string key(1, text[pos + 1]);
+            auto colorIndex = std::find(TextColorsKeys.begin(), TextColorsKeys.end(), key);
+
+            if (colorIndex != TextColorsKeys.end())
+            {
+                size_t colorCodeIndex = std::distance(TextColorsKeys.begin(), colorIndex);
+                const std::string& ansiCode = TextColors[colorCodeIndex];
+
+                text.replace(pos, 2, ansiCode);
+                pos += ansiCode.size();
+            }
+            else
+            {
+                ++pos;
+            }
+        }
+
+        text.append(TextColors[(size_t)TextColor::Default]);
+    }
+};
+
+Logger::Options Logger::m_Options = {};
+
+std::array<std::string, COLORS_COUNT> Logger::TextColorsKeys =
+{
+    "d", // Default
+    "r", // Red
+    "g", // Green
+    "y", // Yellow
+    "b", // Blue
+    "p", // Purple / Magenta
+    "c", // Cyan
+    "w"  // White
+};
+
+std::array<std::string, COLORS_COUNT> Logger::TextColors =
+{
+    "\033[0m",     // Default
+    "\033[0;31m",  // Red
+    "\033[0;32m",  // Green
+    "\033[0;33m",  // Yellow
+    "\033[0;34m",  // Blue
+    "\033[0;35m",  // Purple / Magenta
+    "\033[0;36m",  // Cyan
+    "\033[0;37m",  // White
+};
