90f5a701bb09faeefff3ddd1282446252fad67f9
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2025-09-08 15:39:47 +0300

    Started RE for GameLogic.h
    Got the available resource from Liveoverflow for GameLogic.h
    Added hookchains.h
    Tried some pointer hooking from GameLogic.dll (failed because of ASLR)

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,5 +1,6 @@
 ﻿#include <windows.h>
 #include <iostream>
+#include "sdk/GameLogic.h"
 
 HMODULE g_hOriginal = nullptr;
 
@@ -21,14 +22,27 @@ static DWORD WINAPI InitThread(LPVOID)
 {
     g_hOriginal = LoadLibraryA("gamelogic_original.dll");
 
+    if (g_hOriginal == NULL)
+    {
+        MessageBoxA(NULL, "Failed to find gamelogic_original.dll", "Error", MB_OKCANCEL | MB_ICONERROR);
+        return 0;
+    }
+
     CreateDebugConsole();
+
+    uintptr_t dllBase = (uintptr_t)g_hOriginal;
+
+    uintptr_t worldAddr = dllBase + 0x97D7C;
+    ClientWorld* world = nullptr;
+    world = *(ClientWorld**)worldAddr;
 
     return 0;
 }
 
-static BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID)
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID)
 {
-    if (reason == DLL_PROCESS_ATTACH) {
+    if (reason == DLL_PROCESS_ATTACH)
+    {
         DisableThreadLibraryCalls(hinst);
         CreateThread(nullptr, 0, InitThread, nullptr, 0, nullptr);
     }
