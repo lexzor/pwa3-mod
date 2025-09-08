0839699e041b56c920507f3477521a874bfbc107
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2025-09-08 18:10:51 +0300

    Code cleaning

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -8,9 +8,11 @@ static void CreateDebugConsole()
 {
     AllocConsole();
     FILE* fp;
+
     freopen_s(&fp, "CONOUT$", "w", stdout);
     freopen_s(&fp, "CONOUT$", "w", stderr);
     freopen_s(&fp, "CONIN$", "r", stdin);
+    
     std::cout.clear();
     std::clog.clear();
     std::cerr.clear();
